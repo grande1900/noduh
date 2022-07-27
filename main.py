87d39335790d67a7c626bf9e028c6b956eff22e1
grande1900 <54255077+grande1900@users.noreply.github.com>
2022-07-27 02:32:33 -0400

    added sound and changed path to script folder

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import shutil
 dest = os.getenv("LOCALAPPDATA")+r"\Roblox\Versions\*\content\sounds\\" # Roblox ouch dir
 list_ = glob.glob(dest)
 for i in list_:
-    shutil.copyfile(".\\ouch.ogg",os.path.relpath(i,os.getcwd())+"\\ouch.ogg") # copy file to game
+    shutil.copyfile("ouch.ogg", os.path.relpath(i, os.path.dirname(os.path.realpath(__file__))) + "\\ouch.ogg") # copy file to game
