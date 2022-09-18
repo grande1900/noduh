f446a76efc652de2db5cb0e78dace565f647237c
grande1900 <54255077+grande1900@users.noreply.github.com>
2022-09-18 13:48:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ dest = os.getenv("LOCALAPPDATA")+r"\Roblox\Versions\*\content\sounds\\" # Roblox
 list_ = glob.glob(dest)
 for i in list_:
     shutil.copyfile("ouch.ogg", os.path.relpath(i, os.path.dirname(os.path.realpath(__file__))) + "\\ouch.ogg") # copy file to game
+    shutil.copyfile("ouch.ogg", os.path.relpath(i, os.path.dirname(os.path.realpath(__file__))) + "\\volume_slider.ogg") # also replace volume slider sound
